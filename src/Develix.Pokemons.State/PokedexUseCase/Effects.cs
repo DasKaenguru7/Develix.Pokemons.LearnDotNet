0c9098be86eb455f87ed7740d2c914f60f8a1a14
Prakti Kant <praktikant@fastec.de>
2023-06-06 13:00:03 +0200

    Den Bug behoben. Es wird nun eine Felermeldung angezeigt die sagt, dass das gesuchte pokemon nicht existirt. und man kann jetz wider ein anderes Pokemon suchen wen ein nicht vorhandenes Pokemon sucht.

--- src/Develix.Pokemons.State/PokedexUseCase/Effects.cs
+++ src/Develix.Pokemons.State/PokedexUseCase/Effects.cs
@@ -5,18 +5,36 @@ namespace Develix.Pokemons.State.PokedexUseCase;
 public class Effects
 {
     private readonly PokeApiClient pokeApiClient;
+    private readonly ISnackbarService snackbarService;
 
-    public Effects(PokeApiClient pokeApiClient)
+    public Effects(PokeApiClient pokeApiClient, ISnackbarService snackbarService)
     {
         this.pokeApiClient = pokeApiClient;
+        this.snackbarService = snackbarService;
     }
 
     [EffectMethod]
     public async Task HandleGetPokemonAction(GetPokemonAction action, IDispatcher dispatcher)
     {
-        var pokemon = await pokeApiClient.GetResourceAsync<Pokemon>(action.PokedexId);
-        var species = await pokeApiClient.GetResourceAsync<PokemonSpecies>(action.PokedexId);
-        var resultAction = new GetPokemonResultAction(pokemon, species);
-        dispatcher.Dispatch(resultAction);
+        try
+        {
+            var pokemon = await pokeApiClient.GetResourceAsync<Pokemon>(action.PokedexId);
+            var species = await pokeApiClient.GetResourceAsync<PokemonSpecies>(action.PokedexId);
+            var resultAction = new GetPokemonResultAction(pokemon, species);
+            dispatcher.Dispatch(resultAction);
+        }
+        catch
+        {
+            var resultAction = new GetPokemonErrorAction(action.PokedexId);
+            dispatcher.Dispatch(resultAction);
+        }
+    }
+
+    [EffectMethod]
+    public Task HandleGetPokemonErrorAction(GetPokemonErrorAction action, IDispatcher dispatcher)
+    {
+        var message = $"Das Pokémon {action.PokemonId} existiert nicht!";
+        snackbarService.ShowErrorMessage(message);
+        return Task.CompletedTask;
     }
 }
--- src/Develix.Pokemons.State/PokedexUseCase/PokedexUseCaseActions.cs
+++ src/Develix.Pokemons.State/PokedexUseCase/PokedexUseCaseActions.cs
@@ -4,4 +4,6 @@ namespace Develix.Pokemons.State.PokedexUseCase;
 
 public record GetPokemonAction(int PokedexId);
 
+public record GetPokemonErrorAction(int PokemonId);
+
 public record GetPokemonResultAction(Pokemon Pokemon, PokemonSpecies Species);
