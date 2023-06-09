1b4c85e703ad4b289c7954570d35d0018269ec21
Prakti Kant <praktikant@fastec.de>
2023-06-09 09:45:37 +0200

    Merged PR 115: Farben der App angepasst
    Farben der App angepasst
    
    Related work items: #158

--- src/Develix.Pokemons.UI.Blazor/Shared/MainLayout.razor.cs
+++ src/Develix.Pokemons.UI.Blazor/Shared/MainLayout.razor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using MudBlazor;
 
 namespace Develix.Pokemons.UI.Blazor.Shared;
 
@@ -9,4 +10,17 @@ public partial class MainLayout
     private void DrawerToggle() => drawerOpen = !drawerOpen;
 
     private static string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown version";
+
+    private MudTheme customTheme = new()
+    {
+        Palette = new PaletteLight()
+        {
+            Primary = Colors.DeepPurple.Default,
+            Secondary = Colors.Red.Accent4,
+            AppbarBackground = Colors.Red.Default,
+            Background = Colors.Grey.Lighten3,
+            DrawerBackground = Colors.Grey.Lighten3,
+        },
+    };
+
 }
