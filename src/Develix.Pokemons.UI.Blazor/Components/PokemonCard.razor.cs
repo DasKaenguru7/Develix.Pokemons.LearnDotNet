42baef4439a17b06e3c64e8962d3ba11839e361d
Prakti Kant <praktikant@fastec.de>
2023-06-15 10:20:09 +0200

    Die Basis Statuswerte des gesucheten pokemon werden nun in einem radar diagram angezeigt dadurch enstandene fehler wurden behoben

--- src/Develix.Pokemons.UI.Blazor/Components/PokemonCard.razor.cs
+++ src/Develix.Pokemons.UI.Blazor/Components/PokemonCard.razor.cs
@@ -1,4 +1,5 @@
 ﻿using ApexCharts;
+using Develix.Pokemons.UI.Blazor.Model;
 using Microsoft.AspNetCore.Components;
 using PokeApiNet;
 
@@ -6,7 +7,17 @@ namespace Develix.Pokemons.UI.Blazor.Components;
 
 public partial class PokemonCard
 {
-    private ApexChart<PokemonStat>? chart;
+    private ApexChart<PokemonStatDisplay>? chart;
+    private ApexChartOptions<PokemonStatDisplay> options = new ApexChartOptions<PokemonStatDisplay>()
+    {
+        PlotOptions = new PlotOptions()
+        {
+            Radar = new PlotOptionsRadar()
+            {
+                Size = 88
+            },            
+        }
+    };
 
     [Parameter]
     [EditorRequired]
@@ -108,10 +119,33 @@ public partial class PokemonCard
         }
     }
 
+    private IReadOnlyList<PokemonStatDisplay> GetPokemonStatDisplays(Pokemon pokemon)
+    {
+        return pokemon.Stats
+            .Select(s => GetPokemonStatDisplay(s))
+            .OrderBy(s => s.Order)
+            .ToList();
+    }
+
+    private PokemonStatDisplay GetPokemonStatDisplay(PokemonStat pokemonStat)
+    {
+        var (name, order) = pokemonStat.Stat.Name switch
+        {
+            "hp" => ("HP", 1),
+            "attack" => ("Atk", 2),
+            "defense" => ("Def", 3),
+            "special-attack" => ("Sp.Atk", 6),
+            "special-defense" => ("Sp.Def", 5),
+            "speed" => ("Init", 4),
+            _ => ("?", 7),
+        };
+        return new PokemonStatDisplay(name, pokemonStat.BaseStat, order);
+    }
+
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         await base.OnAfterRenderAsync(firstRender);
-        if (chart is not null)
+        if (chart is not null && Pokemon is not null)
             await chart.UpdateSeriesAsync(true);
     }
 }
